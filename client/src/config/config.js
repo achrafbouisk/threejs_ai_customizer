3b01f0e61ffef785b736ae1c9b9ebbf73344c444
AkramDev <akram.esaaidi@usmba.ac.ma>
2023-05-30 16:01:53 +0100

    first commit

---
+++ client/src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "https://dalle-image-server.onrender.com/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://dalle-image-server.onrender.com/api/v1/dalle",
+  },
+};
+
+export default config;
